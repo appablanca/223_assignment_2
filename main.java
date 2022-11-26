bee0f370ae26b336228b2cd9b274b93936368aad
appablanca <100962965+appablanca@users.noreply.github.com>
2022-11-26 13:11:16 +0300

    Update main.java

--- main.java
+++ main.java
@@ -1,68 +1,70 @@
-import java.util.*;
-
-import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 import java.io.*;
-public class main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        //String filename = sc.next();
-        int arr[] = ReadingFile.readFiles("integers.txt");
-        int n = arr.length;
-        Integer[] sarr = new Integer[n];
-        for(int i = 0; i < n; i++){
-            sarr[i] = arr[i];
-        }
-        System.out.println("Integers are reading from the " + "filename" + " file, the array is:");
-       
-        insertion.show(sarr);
-        Integer sarr3[] = new Integer[n];
-        for(int i = 0; i < n; i++){
-            sarr3[i] = arr[i];
-        }
-  
-        System.out.println("The array has been sorted in increasing order by using the insertion sort algorithm");
-        insertion.sort(sarr);
-        insertion.show(sarr);
+import java.util.*;
 
-        System.out.println("Step 1 has been completed.");
-        System.out.println("");
+public class ReadingFile {
+	public static int[] readFiles(String file) {
+		try {
+			File f = new File(file);
+			Scanner s = new Scanner(f);
+			int n = 0;
 
-        System.out.println("The array of integers that has been sorted in decreasing order by using the insertion sort algorithm:");
-        insertion.rsort(sarr);
-        insertion.show(sarr);
-        System.out.println("Step 2 has been completed.");
-        System.out.println("");
+			while(s.hasNextInt())
+			{
+				n++;
+				s.nextInt();
+			}
+			int S[] = new int[n];
 
-        System.out.println("Doubles are reading from the doubles.txt file, the array is:");
-        double arr2[] = ReadingFile.readFilesD("doubles.txt");
-        int n2 = arr2.length;
-        Double[] sarr2 = new Double[n2];
-        for(int i = 0; i < n2; i++){
-            sarr2[i] = arr2[i];
-        }
-        insertion.show(sarr2);
-        System.out.println("The array of double values has been sorted in increasing order by using the insertion sort algorithm:");
-        insertion.sort(sarr2);
-        insertion.show(sarr2);
-        System.out.println("Step 3 has been completed.");
-        System.out.println("");
+			Scanner s1 = new Scanner(f);
 
-        System.out.println("The original array is:");
-        insertion.show(sarr3);
-        System.out.println("The array of integer values has been sorted in descending order by using the merge sort algorithm is:");
-        merge.sort(sarr3);
+			for(int i =0; i < S.length; i++)
+				S[i] = s1.nextInt();
 
+			return S;
+			
 
+		}
+		catch(Exception e) {
+			return null;
+		}
+	}
+	public static void printArray(Integer[] arr)
 
+	{
+		for (int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
+			System.out.println();
+			
+	}
 
+	public static double[] readFilesD(String file) {
+		try {
+			File f = new File(file);
+			Scanner s = new Scanner(f);
+			int n = 0;
 
+			while(s.hasNextDouble())
+			{
+				n++;
+				s.nextDouble();
+			}
+			double S[] = new double[n];
 
+			Scanner s1 = new Scanner(f);
 
+			for(int i =0; i < S.length; i++)
+				S[i] = s1.nextDouble();
 
+			return S;
+			
 
-        
-        sc.close();
-    }
-    
+		}
+		catch(Exception e) {
+			return null;
+		}
+	}
+	
+	
 }
+	
